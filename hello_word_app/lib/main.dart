577ddd9267d4d0836e3980e42c09470d7d509d64
anyel-ec <paulangel826@gmail.com>
2024-01-09 23:44:19 -0500

    add screen in main

--- hello_word_app/lib/main.dart
+++ hello_word_app/lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
-void main(){
-  runApp(MyApp());
+import 'package:hello_word_app/presentation/screens/counter_screen.dart';
+
+void main() {
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: Scaffold(
-      body: Center(
-        child: Text("Hello World"),
-      )
-    ), 
-  
-  );
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: CounterScreen());
   }
-  
 }
