195baead545335a507a074825cbcf4547612ee39
anyel-ec <paulangel826@gmail.com>
2024-01-14 19:32:02 -0500

    change application status

--- hello_word_app/lib/presentation/screens/counter_screen.dart
+++ hello_word_app/lib/presentation/screens/counter_screen.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+// counterScreen extiende de StatefulWidget
+class CounterScreen extends StatefulWidget {
+  // constructor
   const CounterScreen({super.key});
+
+  // sobreescribir el metodo createState
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: AppBar(
-        title: const Text ('Contador'), 
-        centerTitle: true
+    return Scaffold(
+        // titulo de la app
+        appBar: AppBar(
+            title: const Text('Contador'), // titulo
+            centerTitle: true // centrar el titulo
 
-      ),
-      body: const Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('10', style: TextStyle( fontSize: 160, fontWeight: FontWeight.w100),), 
-          Text('Click: ', style: TextStyle( fontSize: 25))
-        ],
-      ),
-    ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+            ),
+        // cuerpo de la app
+        body:  Center(
+          // centrar el contenido
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$clickCounter',style:  const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              ), // texto
+              const Text('Click: ', style: TextStyle(fontSize: 25)) // texto
+            ],
+          ),
+        ),
+        // boton flotante
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+          clickCounter ++;
+          setState(() {});
 
-        },
-      child: Icon(Icons.add),
-    )
-    );
+
+          },
+          // icono del boton
+          child: const Icon(Icons.add),
+        ));
   }
 }
